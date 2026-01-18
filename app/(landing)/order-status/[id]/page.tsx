88c9062f7a8e40c519509a5c9f65a5ed4892c817
Shafira Aini <blacxaphire0530@gmail.com>
2026-01-18 18:54:07 +0700

    update styling

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -7,7 +7,7 @@ import OrderSubmitted from "../../components/order-status/order-submitted";
 const OrderStatus = () => {
     const [isConfirmed, setIsConfirmed] = useState(false);
     return (
-        <main className="bg-gray-100 min-h-[120vh]">
+        <main className="bg-gray-100 min-h-[100vh]">
         <div className="max-w-5xl mx-auto py-20">
             <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
         </div>
