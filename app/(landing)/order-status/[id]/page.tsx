33ff66be9fa3d5c62265d8e88eaa453481c728e7
Shafira Aini <blacxaphire0530@gmail.com>
2026-01-22 19:56:55 +0700

    update

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -2,6 +2,7 @@ import OrderConfirmed from "../../components/order-status/order-confirmed";
 import OrderSubmitted from "../../components/order-status/order-submitted";
 import { getTransactionById } from "@/app/services/transaction.service";
 import { TPageProps } from "../../product/[id]/page";
+import OrderRejected from "../../components/order-status/order-rejected";
 
 const OrderStatus = async ({params}: TPageProps) => {
     const {id} = await params;
@@ -11,10 +12,12 @@ const OrderStatus = async ({params}: TPageProps) => {
 
     return (
         <main className="bg-gray-100 min-h-screen">
-        <div className="max-w-5xl mx-auto py-20">
+        <div className="max-w-5xl mx-auto pt-40 pb-20">
             <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
         </div>
-        {transaction.status === "paid" ? <OrderConfirmed/> : <OrderSubmitted/>}
+        {transaction.status === "pending" && <OrderSubmitted />}
+        {transaction.status === "paid" && <OrderConfirmed />}
+        {transaction.status === "rejected" && <OrderRejected />}
     </main>
     );
 };
